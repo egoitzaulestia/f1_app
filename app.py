946aa0ddd35c3f1808e93f650ac3ea263d76b553
egoitzaulestia <egoitzaulestia@gmail.com>
2024-10-18 18:04:40 +0200

    New f1 code

--- app.py
+++ app.py
@@ -1,13 +1,57 @@
-# app.py
+# pages/1_Home.py
 
 import streamlit as st
+import base64
 
 st.set_page_config(
-    page_title="Análisis de Datos F1",
+    page_title="Home",
     page_icon="🏎️",
     layout="wide",
 )
 
-st.write("# Bienvenido al Análisis de Datos de Fórmula 1 🏎️")
+# ---------------------------
+# Función para establecer la imagen de fondo
+def set_background(image_file):
+    with open(image_file, "rb") as image:
+        encoded_string = base64.b64encode(image.read()).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/webp;base64,{encoded_string});
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+# Llamar a la función con la ruta de la imagen
+set_background("images/fondoF1_2.jpg")
+
+# ---------------------------
+# Estilizar el título con un fondo negro y texto blanco
+st.markdown("""
+    <br>
+    <div style="background-color: rgba(0, 0, 0); padding: 20px; border-radius: 10px;">
+        <h1 style="color: white; text-align: center;">
+            Bienvenido al Análisis de Datos de Fórmula 1 
+        </h1>
+        <p style="color: white; text-align: center;">
+            Explora diferentes secciones para obtener análisis detallados sobre pilotos, equipos, circuitos y mucho más en el emocionante mundo de la Fórmula 1.
+        </p>
+    </div>
+    <br>
+    """, unsafe_allow_html=True)
 
 st.sidebar.success("Selecciona una página arriba.")
+
+# # Descripción adicional (opcional)
+# st.markdown("""
+#     <div style="background-color: rgba(0, 0, 0, 0.5); padding: 15px; border-radius: 10px;">
+#         <p style="color: white; text-align: center;">
+#             Explora diferentes secciones para obtener análisis detallados sobre pilotos, equipos, circuitos y mucho más en el emocionante mundo de la Fórmula 1.
+#         </p>
+#     </div>
+#     """, unsafe_allow_html=True)
--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,6 +1,7 @@
 # pages/1_Home.py
 
 import streamlit as st
+# import base64
 
 st.set_page_config(
     page_title="Home",
@@ -15,3 +16,23 @@ Bienvenido a la aplicación interactiva de análisis de datos de Fórmula 1. Aqu
 
 Utiliza el menú de navegación para explorar las distintas secciones de la aplicación.
 """)
+
+# # Función para establecer la imagen de fondo
+# def set_background(image_file):
+#     with open(image_file, "rb") as image:
+#         encoded_string = base64.b64encode(image.read()).decode()
+#     css = f"""
+#     <style>
+#     .stApp {{
+#         background-image: url(data:image/png;base64,{encoded_string});
+#         background-size: cover;
+#         background-position: center;
+#         background-repeat: no-repeat;
+#         background-attachment: fixed;
+#     }}
+#     </style>
+#     """
+#     st.markdown(css, unsafe_allow_html=True)
+
+# # Llamar a la función con la ruta de la imagen
+# set_background("images/fondoF1.webp")
