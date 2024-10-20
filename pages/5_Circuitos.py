c3c31956ed3e35f2449ac66f647a4ad27bbd7191
egoitzaulestia <egoitzaulestia@gmail.com>
2024-10-20 20:55:57 +0200

    circuits

--- pages/5_Circuitos.py
+++ pages/5_Circuitos.py
@@ -67,13 +67,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import folium
 from streamlit_folium import st_folium
 import json
-import reverse_geocoder as rg
-import pycountry_convert as pc
 
 st.set_page_config(
     page_title="Análisis de Circuitos",
@@ -105,6 +101,9 @@ with open('data/surface_classification.json', 'r') as f:
 with open('data/permanence_classification.json', 'r') as f:
     permanence_classification = json.load(f)
 
+with open('data/continent_classification.json', 'r') as f:
+    continent_classification = json.load(f)
+
 # Asignar la clasificación de superficies
 circuits['surface'] = circuits['name'].map(surface_classification)
 surface_numeric_mapping = {'Urbano': 1, 'Mixto': 2, 'Asfalto': 3}
@@ -115,36 +114,8 @@ circuits['permanence'] = circuits['name'].map(permanence_classification)
 permanence_numeric_mapping = {'Temporal': 1, 'Permanente': 2}
 circuits['permanence_numeric'] = circuits['permanence'].map(permanence_numeric_mapping)
 
-# ---------------------------
-# Función para obtener el continente a partir de latitud y longitud
-def get_continent(lat, lng):
-    coordinates = (lat, lng)
-    result = rg.search(coordinates)  # Obtener información sobre el país
-    country_code = result[0]['cc']
-
-    try:
-        continent_code = pc.country_alpha2_to_continent_code(country_code)
-        continent_mapping = {
-            'AF': 'África',
-            'NA': 'América del Norte',
-            'SA': 'América del Sur',
-            'AS': 'Asia',
-            'EU': 'Europa',
-            'OC': 'Oceanía'
-        }
-        return continent_mapping.get(continent_code, 'Desconocido')
-    except KeyError:
-        return 'Desconocido'
-
-# Aplicar la función para obtener el continente de cada circuito
-@st.cache_data
-def add_continent_info(df):
-    df['continent'] = df.apply(lambda row: get_continent(row['lat'], row['lng']), axis=1)
-    return df
-
-circuits = add_continent_info(circuits)
-
-# Mapear los continentes a valores numéricos
+# Asignar la clasificación de continentes
+circuits['continent'] = circuits['name'].map(continent_classification)
 continent_numeric_mapping = {
     'África': 1,
     'América del Norte': 2,
@@ -164,72 +135,10 @@ circuits_year = race_circuits[['year', 'circuitId', 'name_x', 'name_y', 'surface
 circuits_year = circuits_year.rename(columns={'name_x': 'race_name', 'name_y': 'circuit_name'})
 
 # ---------------------------
-# Crear gráficos de dona interactivos
-st.write("## Distribución de Tipos de Circuitos por Año")
-
-# Lista de años desde 2024 hacia atrás
-years = sorted(races['year'].unique(), reverse=True)
-selected_year = st.selectbox("Selecciona el año para ver la distribución de circuitos", years)
-
-# Filtrar datos para el año seleccionado
-circuits_year_selected = circuits_year[circuits_year['year'] == selected_year]
-
-# Datos para los gráficos de dona
-# Tipo de Superficie
-surface_counts = circuits_year_selected['surface'].value_counts().reset_index()
-surface_counts.columns = ['surface', 'count']
-
-# Tipo de Duración
-permanence_counts = circuits_year_selected['permanence'].value_counts().reset_index()
-permanence_counts.columns = ['permanence', 'count']
-
-# Distribución Geográfica
-continent_counts = circuits_year_selected['continent'].value_counts().reset_index()
-continent_counts.columns = ['continent', 'count']
-
-# Crear subplots para los gráficos de dona
-fig = make_subplots(rows=1, cols=3, specs=[[{'type':'domain'}, {'type':'domain'}, {'type':'domain'}]])
-
-# Gráfico de dona para Tipo de Superficie
-fig.add_trace(go.Pie(
-    labels=surface_counts['surface'],
-    values=surface_counts['count'],
-    name="Tipo de Superficie",
-    hole=0.4), 1, 1)
-
-# Gráfico de dona para Tipo de Duración
-fig.add_trace(go.Pie(
-    labels=permanence_counts['permanence'],
-    values=permanence_counts['count'],
-    name="Tipo de Duración",
-    hole=0.4), 1, 2)
-
-# Gráfico de dona para Distribución Geográfica
-fig.add_trace(go.Pie(
-    labels=continent_counts['continent'],
-    values=continent_counts['count'],
-    name="Distribución Geográfica",
-    hole=0.4), 1, 3)
-
-# Actualizar títulos y layout
-fig.update_layout(
-    title_text=f"Distribución de Circuitos en {selected_year}",
-    annotations=[
-        dict(text='Superficie', x=0.08, y=0.5, font_size=14, showarrow=False),
-        dict(text='Duración', x=0.50, y=0.5, font_size=14, showarrow=False),
-        dict(text='Geografía', x=0.92, y=0.5, font_size=14, showarrow=False)
-    ],
-    width=1200,
-    height=500,
-)
-
-st.plotly_chart(fig, use_container_width=True)
-
-# ---------------------------
 # Gráfico de los circuitos más utilizados
 st.write("## Circuitos con Mayor Número de Carreras")
 
-circuit_counts = race_circuits['name_y'].value_counts().reset_index()
+circuit_counts = race_circuits['circuit_name'].value_counts().reset_index()
 circuit_counts.columns = ['circuit_name', 'race_count']
 
 fig = px.bar(
